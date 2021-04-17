de1608fd1c69ce1773a934a50e32980316c43c62
Fardoux Benoit <benoit_fardoux@ens.univ-artois.fr>
2021-04-17 16:05:19 +0200

    objet joueurs

--- src/tests/MaDemoIG.java
+++ src/tests/MaDemoIG.java
@@ -13,8 +13,24 @@ public class MaDemoIG {
 		for (int i = 0;i<nombreDeJoueur;i++) {
 			IG.changerNomJoueur(i,((String)param[(i*3)+1].toString())+" ("+((String)param[(i*3)+2])+")");
 			IG.changerImageJoueur(i, ((int)param[(i*3)+3]));
+			
 		}
+		if (nombreDeJoueur == 2) {
+			for (int i=0;i<6;i++){
+				IG.changerObjetJoueur(0,i,i);
+				IG.changerObjetJoueur(1,i+7,i);
+			}
+		}
+		if (nombreDeJoueur == 3) {
+			for (int i=0;i<6;i++){
+				IG.changerObjetJoueur(0,i,i);
+				IG.changerObjetJoueur(1,i+7,i);
+				IG.changerObjetJoueur(2,i+12,i);
+			}
+		}
+		IG.changerPieceHorsPlateau(1, 0);
 		IG.miseAJourAffichage();
+		
 	}
 
 }
