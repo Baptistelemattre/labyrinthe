a90970f71f0f951f370a62aa9e7cef66d4a9cf21
Fardoux Benoit <benoit_fardoux@ens.univ-artois.fr>
2021-04-17 15:37:01 +0200

    parametrage perso OK

--- src/tests/MaDemoIG.java
+++ src/tests/MaDemoIG.java
@@ -11,7 +11,8 @@ public class MaDemoIG {
 		IG.creerFenetreJeu("Demo groupe bricodeur",nombreDeJoueur);
 		IG.rendreVisibleFenetreJeu();
 		for (int i = 0;i<nombreDeJoueur;i++) {
-			IG.changerNomJoueur(i,((String)param[(i*3)+1].toString()));
+			IG.changerNomJoueur(i,((String)param[(i*3)+1].toString())+" ("+((String)param[(i*3)+2])+")");
+			IG.changerImageJoueur(i, ((int)param[(i*3)+3]));
 		}
 		IG.miseAJourAffichage();
 	}
