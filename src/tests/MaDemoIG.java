597c62675409648bfd9712eb6f1965553fff6339
Fardoux Benoit <benoit_fardoux@ens.univ-artois.fr>
2021-04-17 14:08:30 +0200

    Debut démo

---
+++ src/tests/MaDemoIG.java
@@ -0,0 +1,19 @@
+package tests;
+import grafix.interfaceGraphique.IG;
+
+public class MaDemoIG {
+
+	public static void main(String[] args) {
+		// set param
+		Object param[];
+		param = IG.saisirParametres();
+		int nombreDeJoueur = ((Integer)param[0]).intValue();
+		IG.creerFenetreJeu("Demo groupe bricodeur",nombreDeJoueur);
+		IG.rendreVisibleFenetreJeu();
+		for (int i = 0;i<nombreDeJoueur;i++) {
+			IG.changerNomJoueur(i,((String)param[(i*3)+1].toString()));
+		}
+		IG.miseAJourAffichage();
+	}
+
+}
