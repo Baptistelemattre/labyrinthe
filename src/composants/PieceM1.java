0037c7bb8b14367d4709194a1781b2f5a1a91c27
Fardoux Benoit <benoit_fardoux@ens.univ-artois.fr>
2021-05-10 15:05:56 +0200

    fi copy

--- src/composants/PieceM1.java
+++ src/composants/PieceM1.java
@@ -22,7 +22,8 @@ public class PieceM1 extends Piece {
 	 * @return Une copie de la pièce.
 	 */
 	public Piece copy(){
-		Piece piece = new PieceM1();
+		Piece piece= new PieceM1();
+		piece.setPointsEntree(this.getPointEntree(0), this.getPointEntree(1), this.getPointEntree(2), this.getPointEntree(3));
 		return piece;
 	}
 }
