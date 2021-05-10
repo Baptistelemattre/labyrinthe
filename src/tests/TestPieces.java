770b0f755ea37102f888f83e631f923de2f769be
Victor SIN <sin.victor@outlook.fr>
2021-05-10 10:26:03 +0200

    Correction TestPieces.java

--- src/tests/TestPieces.java
+++ src/tests/TestPieces.java
@@ -21,8 +21,16 @@ public class TestPieces{
 		IG.attendreClic();  
         Piece[] Tableau = Piece.nouvellesPieces();
 
-        IG.changerPieceHorsPlateau(Tableau[Tableau.lenght()-1].getModelePiece(), Tableau[Tableau.lenght()-1].getOrientationPiece());
+		int pos = 0;
+        IG.changerPieceHorsPlateau(Tableau[Tableau.lenght()].getModelePiece(), Tableau[Tableau.lenght()].getOrientationPiece());
+		for (int i = 0; i<7;i++){
+			for (int j = 0; j<7;j++){
+				IG.changerPiecePlateau(i,j,Tableau[pos].getModelePiece(),Tableau[pos].getOrientationPiece());
+				pos ++;
+			}
+		}
 		IG.miseAJourAffichage();
+
 
 		for (int i=0; i<4; i++){
 			IG.attendreClic();
@@ -30,7 +38,14 @@ public class TestPieces{
 				Tableau[j].rotation();
 				System.out.println(Tableau[j].toString());
 			}
-		    IG.changerPieceHorsPlateau(Tableau[Tableau.lenght()-1].getModelePiece(), Tableau[Tableau.lenght()-1].getOrientationPiece());
+			pos = 0;
+			for (int n = 0; i<7;i++){
+				for (int j = 0; j<7;j++){
+					IG.changerPiecePlateau(n,j,Tableau[pos].getModelePiece(),Tableau[pos].getOrientationPiece());
+					pos ++;
+				}
+			}
+		    IG.changerPieceHorsPlateau(Tableau[Tableau.lenght()].getModelePiece(), Tableau[Tableau.lenght()].getOrientationPiece());
 			IG.miseAJourAffichage();
 			System.out.println(Tableau[Tableau.lenght()-1].toString());
 		}
